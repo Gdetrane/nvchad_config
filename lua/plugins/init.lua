7722f068f34865ffed93d4eb63ce7d385a44883a
Gdetrane <gdetrane@redhat.com>
2025-03-26 20:30:01 +0100

    Add augment code for AI suggestions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,29 +1,5 @@
 return {
-  {
-    "supermaven-inc/supermaven-nvim",
-    cmd = "SuperMavenStart",
-    config = function()
-      require("supermaven-nvim").setup {
-        keymaps = {
-          accept_suggestion = "<C-h>",
-          clear_suggestion = "<C-]>",
-          accept_word = "<C-j>",
-        },
-        ignore_filetypes = { cpp = true }, -- or { "cpp", }
-        color = {
-          suggestion_color = "#ffffff",
-          cterm = 244,
-        },
-        log_level = "info", -- set to "off" to disable logging completely
-        disable_inline_completion = false, -- disables inline completion for use with cmp
-        disable_keymaps = false, -- disables built in keymaps for more manual control
-        condition = function()
-          return false
-        end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
-      }
-    end,
-  },
-
+  { "augmentcode/augment.vim", event = "VeryLazy" },
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
